ac6b9ed51711f1a9454fefebb74771314ade2d4b
Sam <aaaa@gmail.com>
2022-02-22 13:36:33 +0200

    cool

---
+++ java.js
@@ -0,0 +1,35 @@
+// let buttons = document.querySelectorAll('input')
+
+// let player = null
+// let bot = null
+
+// buttons[0].addEventListener('click', e => 
+// {
+// switch(player) 
+// {
+// case ()
+
+
+// }
+
+console.log("yes sir")
+
+
+// })
+
+
+
+// buttons[1].addEventListener("click", e => 
+// {
+//     console.log(e.target.value)
+// })
+
+// buttons[2].addEventListener("click", e => 
+// {
+//     console.log(e.target.value)
+// })
+
+// buttons[3].addEventListener("click", e => 
+// {
+//     console.log(e.target.value)
+// })
